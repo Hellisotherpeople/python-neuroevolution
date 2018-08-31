dbf993fdcd7afd42fdda86066e632d9f8ca0b0bb
Allen Roush <gedboy2112@gmail.com>
2018-08-31 14:57:06 -0700

    Added animated contour plots to show the decision boundry - code is still garbage tho :)

--- neuroevolution.py
+++ neuroevolution.py
@@ -4,7 +4,7 @@ import random
 import pdb
 #from numba import jit, prange, njit
 from scipy.special import expit
-from sklearn.datasets import make_classification, make_regression, make_moons
+from sklearn.datasets import make_classification, make_regression, make_moons, make_blobs
 import scipy.linalg.blas as FD
 import h5py
 import matplotlib.pyplot as plt
@@ -12,9 +12,10 @@ import matplotlib.animation as animation
 
 
 
-X, y = make_moons(n_samples=100, random_state=43, noise=0)
-# X, y = make_regression(n_samples=1000, n_features=3,
-#            n_informative=1, random_state=43)
+#X, y = make_moons(n_samples=1000, random_state=43, noise=0.2)
+X, y = make_blobs(n_samples=100, centers=2, n_features=2,random_state=42, cluster_std=3.5)
+#X, y = make_regression(n_samples=100, n_features=2, n_informative=1, random_state=43)
+org_y = y
 y = np.asarray([[i] for i in np.absolute(y)])
 # X = np.absolute(X) * 8  # make it a reasonable
 ######################################
@@ -34,16 +35,17 @@ class Neural_Network(object):
         # parameters
         self.inputSize = 2
         self.outputSize = 1
-        self.hiddenSize = 5
+        self.hiddenSize = 20
         self.load_weights_loc = load_weights_loc
         self.save_weights_loc = save_weights_loc
         self.num_population = num_population
         self.number_iterations = number_iterations
         self.current_iteration = 0
         self.graph = graph
+        self.best_solution = 0
         self.w1_population_list = num_population * [None]
         self.w2_population_list = num_population * [None]
-        plt.axis([0, 1000, 0, 1])
+       # plt.axis([0, self.number_iterations, 0, 1])
         # weights
         if self.load_weights_loc:
             h5f = h5py.File(self.load_weights_loc, 'r')
@@ -59,7 +61,7 @@ class Neural_Network(object):
         #self.z = np.dot(X, self.W1)
         self.z = FD.dgemm(alpha=1.0, a=X.T, b=self.W1.T,
                           trans_a=True, trans_b=True)  # MKL fast dot product
-        self.z2 = expit((self.z))  # activation function
+        self.z2 = NN.Elu((self.z))  # ACTIVATION FUNCTION
         # dot product of hidden layer (z2) and second set of 3x1 weights
         #self.z3 = np.dot(self.z2, self.W2)
         self.z3 = FD.dgemm(alpha=1.0, a=self.z2.T,
@@ -123,10 +125,10 @@ class Neural_Network(object):
         # pdb.set_trace()
         best_canidate = np.asarray(values_list)[ind][0]
         if self.graph:
-            plt.scatter(self.current_iteration, best_canidate)
+            plt.scatter(self.current_iteration, best_canidate, c="g", marker=".")
             plt.pause(0.001)
         else:
-            print(best_canidate)
+            print("iteration num " + str(self.current_iteration) + " RMSE " + str(best_canidate))
         w1_canidates = np.asarray(w1_canidates)
         w2_canidates = np.asarray(w2_canidates)
         # print(w1_canidates[1])
@@ -163,7 +165,11 @@ class Neural_Network(object):
             self.w2_parents = list(new_w2_parents)
             self.W1 = self.w1_parents[0]
             self.W2 = self.w2_parents[0]
+            if num == 0 or num % 10 == 0:
+                self.plot_decision_boundary()
+                plt.pause(0.001)
         best = self.forward(X)
+        self.best_solution = best
         if self.save_weights_loc:
             self.save_weights(self.save_weights_loc)
         print("Best solution" + str(best))
@@ -187,13 +193,62 @@ class Neural_Network(object):
         """Compute softmax values for each sets of scores in x."""
         return np.exp(x) / np.sum(np.exp(x), axis=0)
 
+    def Elu(self, x, a=2):
+        """exponential linear unit, from this paper  https://arxiv.org/abs/1511.07289... seems to work quite well"""
+        return np.where(x <= 0, a * (np.exp(x) - 1), x)
+
+    def gaussian(self, x):
+        sq = np.square(x)
+        neg = np.negative(sq)
+        return np.exp(neg)
+
     def save_weights(self, save_weights_loc):
         h5f = h5py.File(save_weights_loc, 'w')
         h5f.create_dataset('weights1', data=self.W1)
         h5f.create_dataset('weights2', data=self.W2)
         h5f.close()
 
-NN = Neural_Network(100, 1000, None, None, True)
+    def plot_decision_boundary(self):
+        # Set min and max values and give it some padding
+        x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+        y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+        h = 0.5
+        # Generate a grid of points with distance h between them
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+        Z = np.c_[xx.ravel(), yy.ravel()] ##I'm not good enough at numpy to say that I just "knew" that this is what I wanted.. thank you stack overflow
+        Z = NN.forward(Z)
+        Z = Z.reshape(xx.shape)
+        # Plot the contour and training examples
+        cp = plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm_r)
+        if self.current_iteration == 0:
+            plt.colorbar(cp)
+        plt.scatter(X[:, 0], X[:, 1], c=org_y, cmap=plt.cm.Spectral)
+
+NN = Neural_Network(100, 1000, "100pt.hdf5", None, False)
+plt.show()
 a = NN.neuroevolve(0.5)
+
+def plot_decision_boundary():
+    # Set min and max values and give it some padding
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    h = 0.1
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = np.c_[xx.ravel(), yy.ravel()] ##I'm not good enough at numpy to say that I just "knew" that this is what I wanted.. thank you stack overflow
+    Z = NN.forward(Z)
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    cp = plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm_r)
+    plt.colorbar(cp)
+    plt.scatter(X[:, 0], X[:, 1], c=org_y, cmap=plt.cm.Spectral)
+
+#plot_decision_boundary()
+
+
+
+#print(gph_x)
+
+#print(X)
 
 #print("Actual Output: \n" + str(y))
